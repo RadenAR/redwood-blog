679580135dd494f0e68ece3e04284adfe690cbb8
RadenAR <radenabdulrahman@gmail.com>
2020-12-22 14:08:10 -0500

    Layout pages and components

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,8 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const HomePage = () => {
+  return <BlogLayout>This is the homepage!</BlogLayout>
+}
+
+export default HomePage
