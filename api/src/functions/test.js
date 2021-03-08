117fd98995f4fce79da1990e43d4f35f2783581d
RadenAR <radenabdulrahman@gmail.com>
2021-03-08 17:59:20 -0500

    Function should work now

--- api/src/functions/test.js
+++ api/src/functions/test.js
@@ -1,12 +1,14 @@
 import appSettings from '../../../appsettings.json'
 
 export const handler = async (event, context) => {
+  const settings = appSettings
+
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
     },
-    body: JSON.parse(appSettings),
+    body: settings,
   }
 }
