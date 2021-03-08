2956fa9689e12f1e80d1b92413f831803ca3b534
RadenAR <radenabdulrahman@gmail.com>
2021-03-08 17:54:26 -0500

    Parse json

--- api/src/functions/test.js
+++ api/src/functions/test.js
@@ -7,6 +7,6 @@ export const handler = async (event, context) => {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
     },
-    body: appSettings,
+    body: JSON.parse(appSettings),
   }
 }
